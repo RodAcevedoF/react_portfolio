12ad1d78a44fdb77bd8bf9654d175382c1e266f7
RodAcevedoF <raacevedof@gmail.com>
2025-07-15 22:51:45 +0200

    fix: sidenav padding

--- src/components/ui/navs/SideNav.tsx
+++ src/components/ui/navs/SideNav.tsx
@@ -17,11 +17,11 @@ export const SideNav = ({ sectionRefs, active }: SideNavWithActiveProps) => {
     >
       <div
         className={`
-          transform -translate-x-[90%] group-hover:translate-x-0
+          transform -translate-x-[75%] group-hover:translate-x-0
           transition-transform duration-300 ease-in-out
-          flex flex-col gap-5
+          flex flex-col gap-6
           border-3 border-l-0 border-blue-400/30 rounded-r-md
-          backdrop-blur-[2rem] p-3 p-l-5 bg-black/30
+          backdrop-blur-[2rem] p-6 bg-black/30
         `}
       >
         {(["bio", "tech", "education", "experience"] as SectionKey[]).map(
@@ -35,14 +35,6 @@ export const SideNav = ({ sectionRefs, active }: SideNavWithActiveProps) => {
                 ? GraduationCap
                 : BriefcaseBusiness;
 
-            /*         key === "bio"
-                ? IdCard
-                : key === "education"
-                ? GraduationCap
-                : key === "experience"
-                ? BriefcaseBusiness
-                : Cpu;
- */
             return (
               <button
                 key={key}
@@ -51,8 +43,8 @@ export const SideNav = ({ sectionRefs, active }: SideNavWithActiveProps) => {
                 aria-label={`Scroll to ${key}`}
               >
                 <Icon
-                  className={`hover:scale-110 transition-all duration-200 hover:text-yellow-200 cursor-pointer ${
-                    active === key ? "text-white scale-125" : "text-blue-400"
+                  className={`hover:scale-110 transition-all duration-200 hover:text-yellow-200 cursor-pointer w-7 h-auto ${
+                    active === key ? "text-white scale-130" : "text-blue-400"
                   }`}
                 />
               </button>
