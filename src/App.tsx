62e645347fdbcf6b18068a77b2c5a552d0189d34
Rodrigo Acevedo <raacevedof@gmail.com>
2025-12-07 15:46:07 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { MailForm, MiniLoader } from './components';
 import { Modal } from './components';
-//const Splash = lazy(() => import('./pages/Splash'));
 const Splash = lazy(() => import('./pages/Splash'));
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
@@ -10,29 +9,29 @@ const NotFound = lazy(() => import('./pages/NotFound'));
 import { Footer, NavBar } from './components';
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <header className='max-w-screen'>
-          <NavBar />
-        </header>
-        <main className='flex flex-col items-center justify-center max-w-[100vw] h-auto bg-inherit'>
-          <Suspense fallback={<MiniLoader />}>
-            <Routes>
-              <Route path='/' element={<Splash />} />
-              <Route path='/home' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </Suspense>
-        </main>
-        <Footer />
-      </Router>
-      <Modal>
-        <MailForm />
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<header className='max-w-screen'>
+					<NavBar />
+				</header>
+				<main className='flex flex-col items-center justify-center max-w-[100vw] h-auto bg-inherit'>
+					<Suspense fallback={<MiniLoader />}>
+						<Routes>
+							<Route path='/' element={<Splash />} />
+							<Route path='/home' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='*' element={<NotFound />} />
+						</Routes>
+					</Suspense>
+				</main>
+				<Footer />
+			</Router>
+			<Modal>
+				<MailForm />
+			</Modal>
+		</>
+	);
 };
 
 export default App;
