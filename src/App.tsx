2791c0b7fbef665edf82a16ec0a768f1e4677d12
Rodrigo Acevedo <raacevedof@gmail.com>
2026-01-06 01:19:58 +0100

    Refactor: Keep Home eager for instant Title, revert children to lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { MailForm, MiniLoader } from './components';
 import { Modal } from './components';
-const Splash = lazy(() => import('./pages/Splash'));
-const Home = lazy(() => import('./pages/Home'));
+import Splash from './pages/Splash';
+import Home from './pages/Home';
 const About = lazy(() => import('./pages/About'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 import { Footer, NavBar } from './components';
+
+const Layout = () => {
+	return (
+		<>
+			<header className='max-w-screen'>
+				<NavBar />
+			</header>
+			<main className='flex flex-col items-center justify-center max-w-[100vw] h-auto bg-inherit'>
+				<Suspense fallback={<MiniLoader />}>
+					<Outlet />
+				</Suspense>
+			</main>
+			<Footer />
+		</>
+	);
+};
 
 const App = () => {
 	return (
 		<>
 			<Router>
-				<header className='max-w-screen'>
-					<NavBar />
-				</header>
-				<main className='flex flex-col items-center justify-center max-w-[100vw] h-auto bg-inherit'>
-					<Suspense fallback={<MiniLoader />}>
-						<Routes>
-							<Route path='/' element={<Splash />} />
-							<Route path='/home' element={<Home />} />
-							<Route path='/about' element={<About />} />
-							<Route path='*' element={<NotFound />} />
-						</Routes>
-					</Suspense>
-				</main>
-				<Footer />
+				<Routes>
+					<Route path='/' element={<Splash />} />
+					<Route element={<Layout />}>
+						<Route path='/home' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='*' element={<NotFound />} />
+					</Route>
+				</Routes>
 			</Router>
 			<Modal>
 				<MailForm />
